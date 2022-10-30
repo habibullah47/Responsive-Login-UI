d337b39e5ecadc2761be03c2ae536f019f0de58c
Mohammed Habibullah <habibullah786official@gmail.com>
2022-10-30 23:57:52 +0600

    responsive design start

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,3 +2,5 @@ import 'package:flutter/animation.dart';
 
 const kPrimaryColor = Color(0xff6f35a5);
 const kPrimaryLightColor = Color(0xfff1e6ff);
+
+const double defaultPadding = 16.0;
--- lib/main.dart
+++ lib/main.dart
@@ -16,9 +16,29 @@ class MyApp extends StatelessWidget {
       title: 'Flutter auth',
       theme: ThemeData(
         primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            elevation: 0,
+            backgroundColor: kPrimaryColor,
+            shape: const StadiumBorder(),
+            maximumSize: const Size(double.infinity, 48),
+            minimumSize: const Size(double.infinity, 48),
+          ),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: kPrimaryColor,
+          iconColor: kPrimaryColor,
+          prefixIconColor: kPrimaryColor,
+          contentPadding: EdgeInsets.symmetric(
+              horizontal: defaultPadding, vertical: defaultPadding),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(30)),
+            borderSide: BorderSide.none,
+          ),
+        ),
       ),
-      home: WelcomeScreen(),
+      home: const WelcomeScreen(),
     );
   }
 }
